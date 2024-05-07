d875dc19cbc18ff3dc01969e23c366e333969cf0
JacobF78 <146984748+JacobF78@users.noreply.github.com>
2024-05-06 19:07:39 -0700

    added lucky blocks and coins

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,24 +4,30 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "test":
-            case "test1":return tiles.createTilemap(hex`0c000c00010101010101010101010101010000000000000000000001010000000000000000000001010001010000000000000001010000000000000000000001010000000000000000000001010000000001010000000001010000000000000000000001010000000000000001010001010000000000000000000001010000000000000000000001010101010101010101010101`, img`
+            case "test1":return tiles.createTilemap(hex`0c000c00010101010101010101010101010000000000000000000001010002000000000000030001010001010000030000020001010000000000000000000001010000000000000000000001010000000301010000000001010000000000000000020001010000000000000001010001010000000000000000000001010002000000000000000001010101010101010101010101`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 
 2 . . . . . . . . . . 2 
+2 . . . . . . . . 2 . 2 
+2 . 2 2 . . 2 . . . . 2 
 2 . . . . . . . . . . 2 
-2 . 2 2 . . . . . . . 2 
 2 . . . . . . . . . . 2 
-2 . . . . . . . . . . 2 
-2 . . . . 2 2 . . . . 2 
+2 . . . 2 2 2 . . . . 2 
 2 . . . . . . . . . . 2 
 2 . . . . . . . 2 2 . 2 
 2 . . . . . . . . . . 2 
 2 . . . . . . . . . . 2 
 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +37,12 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "stone1":
             case "tile1":return tile1;
+            case "collectibleSpawn":
+            case "tile2":return tile2;
+            case "luckyTile":
+            case "tile3":return tile3;
+            case "unluckyTile":
+            case "tile4":return tile4;
         }
         return null;
     })
